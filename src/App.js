06120d3081e1bef6b97cdb58c09e319662d841dc
robbins312 <robbins312@naver.com>
2023-11-27 16:36:09 +0900

    post api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIstLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMovieHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIstLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-https-4b1d6-default-rtdb.firebaseio.com/movies.json"
+      );
       //error handling > catch구문으로 보내짐
       if (!response.ok) {
         throw new Error("SomeThing went wrong!");
@@ -19,20 +23,48 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedData = [];
+
+      for (const key in data) {
+        loadedData.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedData);
     } catch (error) {
       setError(error.message);
       console.log(error);
     }
     setIstLoading(false);
+  }, []);
+  //useEffect 때문에 최초로딩시에도 데이터를 fetch함.
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-https-4b1d6-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <o>Found no movies</o>;
@@ -50,7 +82,10 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
